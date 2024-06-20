5456ed9d05833660e332c87d5b0de28fb8f2258a
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-06-20 16:53:15 +0530

    create errorMiddleware

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,10 +1,10 @@
-
+import asyncHandler from 'express-async-handler';
 // @desc Auth user Token
 //  route POST /api/user/auth
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Auth User" })
-}
+})
 
-export {authUser}
+export { authUser }
 
 
--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv';
 dotenv.config()
 import userRoutes from './routes/userRoutes.js'
 
+import { notFound, errorHandle } from './middleware/errorMiddleware.js';
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use('/api/users',userRoutes)
+app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandle);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port:${PORT}`);
