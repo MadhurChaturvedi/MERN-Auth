06b309ce8eb54375a0889c4c786ce066057607dd
Madhur Chaturvedi <madhurchaturvedi2000@gmail.com>
2024-06-22 01:38:32 +0530

    create user logedout contoller means cookee delete session expire

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -58,7 +58,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route POST /api/user/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Logout User" })
+
+    res.cookie('jwt','',{
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({ message: "User Logout" })
 })
 
 // @desc Get user Profile
